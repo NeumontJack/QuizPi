0156f90a1b0805745c2943ecfcf252f3e597c5ca
JMHere <jmendez@student.neumont.edu>
2024-05-14 21:08:10 -0600

    DB Connection
    This is using my info will have to change on other peoples end

--- QuizPi/QuizPi/BackEnd/dbConnection.php
+++ QuizPi/QuizPi/BackEnd/dbConnection.php
@@ -1,1 +1,27 @@
 <?php
+
+DEFINE("SERVER", "localhost");
+DEFINE("USERN", "root");
+DEFINE("PASS", "Dr.Phid21@");
+DEFINE("DBNAME", ""); // Need to determine what the db name to be
+
+// Try connection with db
+function dbconnect()
+{
+    $dbConn = mysqli_connect(SERVER, USERN, PASS, DBNAME);
+
+    if (!$dbConn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    return $dbConn;
+}
+
+function getAllRecords($dbConn, $info) 
+{
+    $query = "";
+
+    return mysqli_query($dbConn, $query);
+}
+
+?>
