ed787e22b671e550cc14e790c80f4a5a86b0cdd9
NeumontJack <100381502+NeumontJack@users.noreply.github.com>
2024-05-19 16:41:36 -0600

    Update adminHome.php

--- QuizPi/QuizPi/FrontEnd/adminHome.php
+++ QuizPi/QuizPi/FrontEnd/adminHome.php
@@ -25,28 +25,13 @@ include('db.php');
 
         <div class="admin-section">
             <h3>Manage Quizzes</h3>
-            <a href="admin_quiz.php?action=create" class="btn">Create New Quiz</a>
-            <a href="admin_quiz.php?action=view" class="btn">View/Edit Quizzes</a>
+            <a href="Quiz.php?action=create" class="btn">Create New Quiz</a>
+            <a href="Quiz.php?action=view" class="btn">View/Edit Quizzes</a>
         </div>
 
         <div class="admin-section">
             <h3>Manage Scores</h3>
-            <a href="admin_scoreboard.php" class="btn">View/Edit Scores</a>
-        </div>
-
-        <div class="admin-section">
-            <h3>Categories</h3>
-            <a href="admin_categories.php" class="btn">Manage Categories</a>
-        </div>
-
-        <div class="admin-section">
-            <h3>Admin Settings</h3>
-            <a href="admin_settings.php" class="btn">Settings</a>
-        </div>
-
-        <div class="admin-section">
-            <h3>Logout</h3>
-            <a href="logout.php" class="btn">Logout</a>
+            <a href="Scoreboard.php" class="btn">View/Edit Scores</a>
         </div>
     </div>
 
