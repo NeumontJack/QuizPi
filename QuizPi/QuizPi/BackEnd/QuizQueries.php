1310ce37a130347fcf0986a7452ae72ffb674e91
JMHere <jmendez@student.neumont.edu>
2024-05-20 15:19:40 -0600

    Changes to the create in Queries

--- QuizPi/QuizPi/BackEnd/QuizQueries.php
+++ QuizPi/QuizPi/BackEnd/QuizQueries.php
@@ -46,42 +46,40 @@ if(array_key_exists('name', $_POST) == TRUE)
             header('Location: ../FrontEnd/adminHome.php');
         }
     }
+}
 
-    if (array_key_exists('Sname', $_POST) == TRUE) {
+if (array_key_exists('Sname', $_POST) == TRUE) {
 
-        $username = $_POST['Sname'];
-        $password = $_POST['Spassword'];
+    $username = $_POST['Sname'];
+    $password = $_POST['Spassword'];
 
-        $dbConn = dbconnect();
+    $dbConn = dbconnect();
 
-        addNewUser($dbConn, $username, $password);
+    addNewUser($dbConn, $username, $password);
 
-        $feedBack = getLoginConformation($dbConn, $username, $password);
+    $feedBack = getLoginConformation($dbConn, $username, $password);
 
-        if ($feedBack) {
-            // loop through each record and format the json (apply any needed business logic)
-            while ($row = mysqli_fetch_array($feedBack)) {
-                $rowArray[] = json_decode($row[0]);
-            }
-        }
-        mysqli_close($dbConn);
-        if ($rowArray[0]->id == null) {
-
-            header('Location: ../FrontEnd/index.php');
-
-        } else {
-            //echo $rowArray[0]->role;
-            if ($rowArray[0]->role == "USER") {
-                $_SESSION['name'] = $rowArray[0]->userN;
-                $_SESSION['pass'] = $rowArray[0]->pass;
-                header('Location: ../FrontEnd/userHome.php'); //?credname=' . $rowArray[0]->userN);
-            } else {
-                header('Location: ../FrontEnd/adminHome.php');
-            }
+    if ($feedBack) {
+        // loop through each record and format the json (apply any needed business logic)
+        while ($row = mysqli_fetch_array($feedBack)) {
+            $rowArray[] = json_decode($row[0]);
         }
     }
-    
-    
+    mysqli_close($dbConn);
+    if ($rowArray[0]->id == null) {
+
+        header('Location: ../FrontEnd/index.php');
+
+    } else {
+        //echo $rowArray[0]->role;
+        if ($rowArray[0]->role == "USER") {
+            $_SESSION['name'] = $rowArray[0]->userN;
+            $_SESSION['pass'] = $rowArray[0]->pass;
+            header('Location: ../FrontEnd/userHome.php'); //?credname=' . $rowArray[0]->userN);
+        } else {
+            header('Location: ../FrontEnd/adminHome.php');
+        }
+    }
 }
 
 ?>
